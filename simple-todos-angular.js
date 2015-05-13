7037614aa24edbf7f2f5f1b8b44b8d813d604695
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-05-13 16:03:07 -0700

    Step 4: A form to add tasks, sort to make newest tasks at the top

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -8,7 +8,16 @@ if (Meteor.isClient) {
   angular.module("simple-todos").controller("TodosListCtrl", ['$scope', '$meteor',
     function($scope, $meteor){
 
-      $scope.tasks = $meteor.collection(Tasks);
+      $scope.tasks = $meteor.collection(function(){
+        return Tasks.find({}, {sort: {createdAt: -1}})
+      });
+
+      $scope.addTask = function(newTask){
+        $scope.tasks.push({
+            text: newTask,
+            createdAt: new Date()}
+        );
+      };
 
     }]);
 }

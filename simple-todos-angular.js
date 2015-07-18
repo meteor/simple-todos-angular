cbbf8aed7c84eaecbf34b72863945924211728e4
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-07-18 15:02:50 +0300

    Step 9.2: Add Meteor Methods for add, delete and check tasks

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -47,3 +47,25 @@ if (Meteor.isClient) {
 
     }]);
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});

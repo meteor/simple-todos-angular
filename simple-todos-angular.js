7d3a03a08b48b1a1f0ac492d7f6685206b459d36
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-07-18 13:01:36 +0300

    Step 4.3: Sort task list by date

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -8,7 +8,9 @@ if (Meteor.isClient) {
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
 
-      $scope.tasks = $meteor.collection(Tasks);
+      $scope.tasks = $meteor.collection( function() {
+        return Tasks.find({}, { sort: { createdAt: -1 } })
+      });
 
       $scope.addTask = function (newTask) {
         $scope.tasks.push( {

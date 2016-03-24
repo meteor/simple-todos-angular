83c53904e21e0aa21f236193532bb81075929d00
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-03-24 18:31:22 +0200

    Step 11.10: Add test to check form reset

--- imports/components/todosList/client/todosList.tests.js
+++ imports/components/todosList/client/todosList.tests.js
@@ -51,6 +51,10 @@ describe('todosList', function() {
         sinon.assert.calledOnce(Meteor.call);
         sinon.assert.calledWith(Meteor.call, 'tasks.insert', newTask);
       });
+
+      it('should reset newTask', function() {
+        assert.equal(controller.newTask, '');
+      });
     });
   });
 })

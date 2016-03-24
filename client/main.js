9d407e878bfc21b7be80bda6361f70a8b8e4f109
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-03-24 18:08:45 +0200

    Step 6.2: Bootstrap Angular to mobile as well

--- client/main.js
+++ client/main.js
@@ -6,3 +6,13 @@ angular.module('simple-todos', [
   angularMeteor,
   todosList.name
 ]);
+
+function onReady() {
+  angular.bootstrap(document, ['simple-todos']);
+}
+
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}

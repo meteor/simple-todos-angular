f2ae55fd1d12073751f0d2a311b2ae7079f0ddfa
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-03-24 18:14:35 +0200

    Step 8.5: Import accounts configuration

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todosList from '../imports/components/todosList/todosList';
+import '../imports/startup/accounts-config.js';
 
 angular.module('simple-todos', [
   angularMeteor,

24d5b599f22aef3e34782d2937667576be60154d
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-03-24 18:09:56 +0200

    Step 7.2: Add reactive query parameter

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -8,10 +8,21 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.hideCompleted = false;
+
     this.helpers({
       tasks() {
+        const selector = {};
+
+        // If hide completed is checked, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
         // Show newest tasks at the top
-        return Tasks.find({}, {
+        return Tasks.find(selector, {
           sort: {
             createdAt: -1
           }

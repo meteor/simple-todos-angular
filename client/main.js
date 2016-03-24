12ffe2d30b1db1e4c7ca94fe830ba4ea0e28162b
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-03-24 18:00:48 +0200

    Step 2.7: Add todosList component to app

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
 
 angular.module('simple-todos', [
-  angularMeteor
+  angularMeteor,
+  todosList.name
 ]);

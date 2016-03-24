469977de20414ad5442833b3b0e6ae3461c31291
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-03-24 18:15:14 +0200

    Step 8.6: Update insert to include user data

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../../api/tasks.js';
 
 import template from './todosList.html';
@@ -42,7 +43,9 @@ class TodosListCtrl {
     // Insert a task into the collection
     Tasks.insert({
       text: newTask,
-      createdAt: new Date
+      createdAt: new Date,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
     });
 
     // Clear form

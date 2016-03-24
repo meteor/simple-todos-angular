eeec7ea8a21aee5dad65cfc9ff480413d07fcb52
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-03-24 17:56:48 +0200

    Step 2.3: Replace starter JS code

--- client/main.js
+++ client/main.js
@@ -1,22 +1,6 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+angular.module('simple-todos', [
+  angularMeteor
+]);

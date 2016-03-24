c637635457f0c0e5fe6af6fc5e4a7d708dac25d2
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-03-24 18:07:19 +0200

    Step 5.2: Add methods for buttons

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -30,6 +30,19 @@ class TodosListCtrl {
     // Clear form
     this.newTask = '';
   }
+
+  setChecked(task) {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+
+  removeTask(task) {
+    Tasks.remove(task._id);
+  }
 }
 
 export default angular.module('todosList', [
